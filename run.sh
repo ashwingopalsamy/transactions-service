e31cde59d14f3325a12e2f637627c47040feb14b
Ashwin Gopalsamy <47941624+ashwingopalsamy@users.noreply.github.com>
2025-02-08 09:14:37 +0530

    docs: enhance service documentation README (#8)
    # Changes
    
    With this PR, the following improvements are proposed:
    
    1. A single-command bash script that builds and runs the service incl.
    its dependencies
    2. Enhanced service documentation with improved README.md
    3. Added few handy badges for improved experience.

---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+echo "Note: Requires Docker and Docker Compose Installed!"
+
+# Variables
+SERVICE_NAME="app"
+IMPORT_PATH="github.com/ashwingopalsamy/transactions-service"
+PKG_SRC="${IMPORT_PATH}/cmd/${SERVICE_NAME}"
+BUILD_TAG="local"
+IMAGE="${SERVICE_NAME}:${BUILD_TAG}"
+MIGRATION_IMAGE="migration_builder:${BUILD_TAG}"
+DB_DSN="postgres://postgres:postgres@db:5432/postgres?sslmode=disable"
+DOCKER_COMPOSE="docker compose --file docker-compose.yml"
+
+export IMAGE
+export MIGRATION_IMAGE
+
+echo "Handling dependencies..."
+go mod tidy
+go mod vendor
+
+echo "Building the service..."
+docker build --pull --tag "${IMAGE}" \
+    --build-arg importPath="${IMPORT_PATH}" \
+    --build-arg pkg="${PKG_SRC}" .
+
+echo "Building the migration image..."
+docker build --tag "${MIGRATION_IMAGE}" \
+    --build-arg importPath="${IMPORT_PATH}" \
+    -f schema/migrations.Dockerfile .
+
+echo "Running database migrations..."
+${DOCKER_COMPOSE} run --rm migrations -dir /migrations postgres "${DB_DSN}" up
+
+echo "Starting all services..."
+${DOCKER_COMPOSE} up --build -d
+${DOCKER_COMPOSE} logs -f
